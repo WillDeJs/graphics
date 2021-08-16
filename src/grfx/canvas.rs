14365ad93d40d9dd2e9f8c1411b261618ed41059
willdejs <wilrad1691@gmail.com>
2021-08-16 19:58:07 -0400

    moved assets

--- src/grfx/canvas.rs
+++ src/grfx/canvas.rs
@@ -616,7 +616,7 @@ impl Canvas {
 fn read_font() -> Option<HashMap<char, Sprite>> {
     let mut font_map = HashMap::<char, Sprite>::new();
 
-    match PNGImage::from_file("../assets/font2.png") {
+    match PNGImage::from_file("./assets/font2.png") {
         Ok(image) => {
             let extractor =
                 SpriteExtractor::from_png(&image, SpriteSize::new(50, 85), 0, 15).unwrap();
